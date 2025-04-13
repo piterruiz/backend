c1fa26345a870e2a44af6c63531f3a0f2843217f
Evensoft <yon.sena.adso@gmail.com>
2025-04-13 11:54:11 -0500

    Creacion Hojasvida

--- app/Http/Controllers/Api/hojasvidaController.php
+++ app/Http/Controllers/Api/hojasvidaController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Genero;
+use App\Models\Hojasvida;
 use Illuminate\Support\Facades\Validator;
 
-class generoController extends Controller
+class hojasvidaController extends Controller
 {
     public function index()
     {
-        $generos = Genero::all();
+        $Hojasvidas = Hojasvida::all();
         $data = [
-            "rol" => $generos,
+            "rol" => $Hojasvidas,
             "status" => 200
         ];
         return response()->json($data, 200);
@@ -21,13 +21,14 @@ class generoController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            "nombreGenero" => "required|min:3|max:30",
-            "abreviacionGenero" => "required|max:2"
+            "claseLibretaMilitar" => "required|max:45",
+            "numeroLibretaMilitar" => "required|max:45",
+            "usuarioNumDocumento" => "required|number"
         ]);
 
         if ($validator->fails()) {
             $data = [
-                "mensaje" => "Error en la validación de géneros",
+                "mensaje" => "Error en la validación de Hojas de vida",
                 "errors" => $validator->errors(),
                 "status" => 400
             ];
@@ -35,14 +36,15 @@ class generoController extends Controller
         }
 
         try {
-            $genero = Genero::create([
-                "nombreGenero" => $request->nombreGenero,
-                "abreviacionGenero" => $request->abreviacionGenero
+            $Hojasvida = Hojasvida::create([
+                "claseLibretaMilitar" => $request->claseLibretaMilitar,
+                "numeroLibretaMilitar" => $request->numeroLibretaMilitar,
+                "usuarioNumDocumento " => $request->usuarioNumDocumento
             ]);
 
             return response()->json([
-                "mensaje" => "Género creado correctamente",
-                "genero" => $genero,
+                "mensaje" => "Hoja de vida creada correctamente",
+                "Hojasvida" => $Hojasvida,
                 "status" => 201
             ], 201);
         } catch (\Exception $e) {
@@ -56,50 +58,51 @@ class generoController extends Controller
 
     public function show($id)
     {
-        $genero = Genero::find($id);
-        if (!$genero) {
+        $Hojasvida = Hojasvida::find($id);
+        if (!$Hojasvida) {
             $data = [
-                "mensage" => " No se encontro genero",
+                "mensage" => " No se encontro Hojasvida",
                 "status" => 201
             ];
             return response()->json([$data], 201);
         }
         $data = [
-            "rol" => $genero,
+            "rol" => $Hojasvida,
             "status" => 200
         ];
         return response()->json([$data], 200);
     }
     public function destroy($id)
     {
-        $genero = Genero::find($id);
-        if (!$genero) {
+        $Hojasvida = Hojasvida::find($id);
+        if (!$Hojasvida) {
             $data = [
-                "mensage" => " No se encontro genero",
+                "mensage" => " No se encontro Hoja de vida",
                 "status" => 404
             ];
             return response()->json([$data], 404);
         }
-        $genero->delete();
+        $Hojasvida->delete();
         $data = [
-            "rol" => 'Genero eliminado',
+            "rol" => 'Hoja de vida eliminada',
             "status" => 200
         ];
         return response()->json([$data], 200);
     }
     public function update(Request $request, $id)
     {
-        $genero = Genero::find($id);
-        if (!$genero) {
+        $Hojasvida = Hojasvida::find($id);
+        if (!$Hojasvida) {
             $data = [
-                "mensage" => " No se encontro Genero",
+                "mensage" => " No se encontro Hoja de vida",
                 "status" => 404
             ];
             return response()->json([$data], 404);
         }
         $validator = Validator::make($request->all(), [
-            "nombreGenero" => "required|min:3|max:30",
-            "abreviacionGenero" => "required|max:2"
+            "claseLibretaMilitar" => $request->claseLibretaMilitar,
+            "numeroLibretaMilitar" => $request->numeroLibretaMilitar,
+            "usuarioNumDocumento " => $request->usuarioNumDocumento
         ]);
         if ($validator->fails()) {
             $data = [
@@ -108,28 +111,28 @@ class generoController extends Controller
             ];
             return response()->json([$data], 400);
         }
-        $genero->nombreGenero = $request->nombreGenero;
-        $genero->abreviacionGenero = $request->abreviacionGenero;
+        $Hojasvida->claseLibretaMilitar = $request->claseLibretaMilitar;
+        $Hojasvida->numeroLibretaMilitar = $request->numeroLibretaMilitar;
+        $Hojasvida->usuarioNumDocumento = $request->usuarioNumDocumento;
         try {
-            $genero->save();
+            $Hojasvida->save();
             $data = [
-                "rol" => $genero,
+                "hojasvida" => $Hojasvida,
                 "status" => 200
             ];
             return response()->json([$data], 200);
         } catch (\Exception $e) {
             return response()->json([
-                "mensaje" => "Error al modificar el rol",
+                "mensaje" => "Error al modificar la hoja de vida",
                 "error" => $e->getMessage(),
                 "status" => 500
             ], 500);
-            
         }
     }
     public function updatePartial(Request $request, $id)
     {
-        $genero = Genero::find($id);
-        if (!$genero) {
+        $Hojasvida = Hojasvida::find($id);
+        if (!$Hojasvida) {
             $data = [
                 "mensage" => " No se encontro rol",
                 "status" => 404
@@ -137,29 +140,33 @@ class generoController extends Controller
             return response()->json([$data], 404);
         }
         $validator = Validator::make($request->all(), [
-            "nombreGenero" => "min:3|max:30",
-            "abreviacionGenero" => "max:2"
+            "claseLibretaMilitar" => "max:45",
+            "numeroLibretaMilitar" => "max:45",
+            "usuarioNumDocumento" => "number"
         ]);
         if ($validator->fails()) {
             $data = [
-                "mesaje " => "Error al validar rol",
+                "mesaje " => "Error al validar Hoja de vida",
                 "errors" => $validator->errors(),
                 "status" => 400
             ];
             return response()->json([$data], 400);
         }
-        if ($request->has("idGenero")) {
-            $genero->idGenero = $request->idGenero;
+        if ($request->has("idHojaDeVida")) {
+            $Hojasvida->idHojasvida = $request->idHojasvida;
         }
-        if ($request->has("nombreGenero")) {
-            $genero->nombreGenero = $request->nombreGenero;
+        if ($request->has("claseLibretaMilitar")) {
+            $Hojasvida->claseLibretaMilitar = $request->claseLibretaMilitar;
         }
-        if ($request->has("abreviacionGenero")) {
-            $genero->abreviacionGenero = $request->abreviacionGenero;
+        if ($request->has("numeroLibretaMilitar")) {
+            $Hojasvida->numeroLibretaMilitar = $request->numeroLibretaMilitar;
         }
-        $genero->save();
+        if ($request->has("usuarioNumDocumento")) {
+            $Hojasvida->usuarioNumDocumento = $request->usuarioNumDocumento;
+        }
+        $Hojasvida->save();
         $data = [
-            "rol" => $genero,
+            "rol" => $Hojasvida,
             "status" => 200
         ];
         return response()->json([$data], 200);
